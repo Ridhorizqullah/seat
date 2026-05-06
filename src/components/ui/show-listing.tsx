a47c56d449dc972eba1d2a6a1e4a55975e1e4e6f
Ridhorzqullah <ridhorizqullah3@gmail.com>
2026-05-06 22:41:19 +0700

    chore: remove unused Performance import from show-listing

--- src/components/ui/show-listing.tsx
+++ src/components/ui/show-listing.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { formatDate, formatTime, formatPrice } from '../../lib/utils'
-import { Show, Performance } from '../../types'
+import { Show } from '../../types'
 import { Button } from './button'
 import { cn } from '../../lib/utils'
 
